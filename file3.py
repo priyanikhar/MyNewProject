a4b2c7875b7003ceb0cf826c3c70e1ecee3d827b
priyanikhar <priyanikhar167@gmail.com>
2026-05-17 17:55:33 +0530

    commit 04

---
+++ file3.py
@@ -0,0 +1,13 @@
+def vowel_count():
+    count=0
+    username=input("enter your name:").lower()
+    for char in usernmae:
+        if char in "aeiou":
+            count+=1
+    return count
+vowel_count()    
+    
+        
+    
+    
+    
